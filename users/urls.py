a29fda4da4e8513e9a73570808daec94b3c5e7ba
Ali <alisher.abdimuminov.2005@gmail.com>
2025-07-07 12:08:33 +0500

    bug fix

---
+++ users/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+
+from .views import (
+    login,
+    signup,
+    delete,
+
+    profile,
+    edit_profile,
+    get_invites,
+    accept_invite,
+    
+    create_group,
+    get_students,
+    invite_members,
+)
+
+
+urlpatterns = [
+    path("auth/login/", login),
+    path("auth/signup/", signup),
+    path("auth/delete/", delete),
+
+    path("auth/profile/", profile),
+    path("auth/profile/edit/", edit_profile),
+    path("auth/profile/invites/", get_invites),
+    path("auth/profile/invites/accept/", accept_invite),
+
+    path("teacher/group/create/", create_group),
+    path("teacher/students/", get_students),
+    path("teacher/group/invite/", invite_members),
+]
