1faade73c8cb3fcc76729553b1ffe4853534035c
Ali <alisher.abdimuminov.2005@gmail.com>
2025-07-29 14:39:47 +0500

    bug fix

--- tests/urls.py
+++ tests/urls.py
@@ -10,6 +10,8 @@ from .views import (
     SubjectsListAPIView,
     BannersListAPIView,
     get_statistics,
+    purchase_cefr,
+    purchase_dtm,
 )
 
 
@@ -20,10 +22,12 @@ urlpatterns = [
     path("dtms/", DtmsListAPIView.as_view()),
     path("dtms/dtm/<int:pk>/", get_dtm),
     path("dtms/dtm/<int:pk>/join/", join_dtm),
+    path("dtms/dtm/<int:pk>/purchase/", purchase_dtm),
 
     path("cefrs/", CefrListAPIView.as_view()),
     path("cefrs/cefr/<int:pk>/", get_cefr),
     path("cefrs/cefr/<int:pk>/join/", join_cefr),
+    path("cefrs/cefr/<int:pk>/purchase/", purchase_cefr),
 
     path("statistics/", get_statistics),
 ]
